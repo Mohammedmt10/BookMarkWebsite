2a46004490a281ac4ea152e9a9aba78ff2c1cb23
Mohammed <mtajir903@gmail.com>
2025-03-23 10:20:23 +0530

    updating the website

--- BookMark-Project/src/components/Main.tsx
+++ BookMark-Project/src/components/Main.tsx
@@ -1,6 +1,6 @@
 export function Main() { 
     return <div className="flex justify-between pr-28 max-w-screen">
-        <div className=" p-12">
+        <div className="ml-12">
             <div className="text-6xl font-bold">
                 A Simple Bookmark <br />
                 Manager
@@ -13,12 +13,12 @@ export function Main() {
             </div>
             <div className="mt-10">
                 <button className="border-2 cursor-pointer font-bold border-[#5368df] px-5 bg-[#5368df] text-white py-3 rounded hover:bg-white hover:text-[#5368df]">Get it On Chrome</button>
-                <button className="font-bold ml-4 border-2 cursor-pointer border-[#d1d5db] px-5 bg-[#d1d5db] py-3 rounded hover:bg-white">Get it On Chrome</button>
+                <button className="font-bold ml-4 border-1 cursor-pointer border-[#d1d5db] px-5 bg-[#d1d5db] shadow-[#dae0e8] shadow-md py-3 rounded hover:bg-white">Get it On Chrome</button>
             </div>
         </div>
         <div className="mx-auto relative p-6">
-            <img src="https://tailwindfromscratch.com/website-projects/bookmark/images/illustration-hero.svg" alt="" className="relative z-[999] ml-24"/>
-            <div className="z-[0] w-[9999px] left-[30%] max-w- rounded-l-full ml-48 min-h-64 -translate-y-60 absolute bg-[#5368df]"></div>
+        <div className="z-[0] translate-x-96 top-68 min-w-xl rounded-l-full min-h-64 absolute bg-[#5368df]"></div>
+            <img src="https://tailwindfromscratch.com/website-projects/bookmark/images/illustration-hero.svg" alt="" className="relative z-10 lg:top-24 xl:top-0 overflow-x-visible"/>
         </div>
     </div>
 }
--- BookMark-Project/src/components/Questions.tsx
+++ BookMark-Project/src/components/Questions.tsx
@@ -3,10 +3,7 @@ import { Arrow } from "../icons/Arrow";
 
 export function Questions() {
 
-    const [open , setOpen] = useState(false)
-    const [open1 , setOpen1] = useState(false)
-    const [open2 , setOpen2] = useState(false)
-    const [open3 , setOpen3] = useState(false)
+    const [question , setQuestion] = useState("");
 
     return <div className="mx-auto mb-40">
         <div className="text-center mb-10">
@@ -19,43 +16,39 @@ export function Questions() {
             </div>
         </div>
         <div onClick={()=>{
-            setOpen(c=>!c)
-            console.log(open)
+            setQuestion(question ?  "" : "Q1");
         }} className={`flex justify-center mx-auto items-center ${!open ? "border-b-2" : ""} border-gray-200 w-fit`}>
                 <div className="duration-300 min-w-2xl p-3 text-lg text-gray-500  hover:text-red-400  cursor-pointer">What is Bookmark?</div>
-                <div className={`${open ? "rotate-180" : ""}`}><Arrow /></div>
+                <div className={`${question == "Q1" ? "rotate-180" : ""}`}><Arrow /></div>
         </div>
-        {open && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
+        {question == "Q1"  && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nihil sapiente autem distinctio cupiditate dolorem debitis pariatur animi iste iusto. Fugit, aliquam porro. Officia delectus ut illum, nisi magni beatae?
         </div>}
         <div onClick={()=>{
-            setOpen1(c=>!c)
-            console.log(open)
-        }} className={`flex justify-center mx-auto items-center ${!open1 ? "border-b-2" : ""} border-gray-200 w-fit`}>
+            setQuestion((question ?  "" : "Q2"))
+        }} className={`flex justify-center mx-auto items-center ${!(question == "Q2") ? "border-b-2" : ""} border-gray-200 w-fit`}>
                 <div className="duration-300 min-w-2xl p-3 text-lg text-gray-500  hover:text-red-400  cursor-pointer">How can I request a new browser?</div>
-                <div className={`${open1 ? "rotate-180" : ""}`}><Arrow /></div>
+                <div className={`${question == "Q2"  ? "rotate-180" : ""}`}><Arrow /></div>
         </div>
-        {open1 && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
+        {question == "Q2" && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nihil sapiente autem distinctio cupiditate dolorem debitis pariatur animi iste iusto. Fugit, aliquam porro. Officia delectus ut illum, nisi magni beatae?
         </div>}
         <div onClick={()=>{
-            setOpen2(c=>!c)
-            console.log(open)
-        }} className={`flex justify-center mx-auto items-center ${!open2 ? "border-b-2" : ""} border-gray-200 w-fit`}>
+            setQuestion((question ?  "" : "Q3"))
+        }} className={`flex justify-center mx-auto items-center ${!(question == "Q3")  ? "border-b-2" : ""} border-gray-200 w-fit`}>
                 <div className="duration-300 min-w-2xl p-3 text-lg text-gray-500  hover:text-red-400  cursor-pointer">Is ther a mobile app?</div>
-                <div className={`${open2 ? "rotate-180" : ""}`}><Arrow /></div>
+                <div className={`${question == "Q3"  ? "rotate-180" : ""}`}><Arrow /></div>
         </div>
-        {open2 && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
+        {question == "Q3"  && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nihil sapiente autem distinctio cupiditate dolorem debitis pariatur animi iste iusto. Fugit, aliquam porro. Officia delectus ut illum, nisi magni beatae?
         </div>}
         <div onClick={()=>{
-            setOpen3(c=>!c)
-            console.log(open)
-        }} className={`flex justify-center mx-auto items-center ${!open3 ? "border-b-2" : ""} border-gray-200 w-fit`}>
+            setQuestion((question ?  "" : "Q4"))
+        }} className={`flex justify-center mx-auto items-center ${!(question == "Q4") ? "border-b-2" : ""} border-gray-200 w-fit`}>
                 <div className="duration-300 min-w-2xl p-3 text-lg text-gray-500  hover:text-red-400  cursor-pointer">What about other Chromium browsers</div>
-                <div className={`${open3 ? "rotate-180" : ""}`}><Arrow /></div>
+                <div className={`${(question == "Q4") ? "rotate-180" : ""}`}><Arrow /></div>
         </div>
-        {open3 && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
+        {(question == "Q4") && <div className="justify-center mx-auto max-w-2xl border-b-2 border-gray-200 py-3">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nihil sapiente autem distinctio cupiditate dolorem debitis pariatur animi iste iusto. Fugit, aliquam porro. Officia delectus ut illum, nisi magni beatae?
         </div>}
     </div>
